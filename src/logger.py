191e70717fbe046597f80a60e49a4e62e26f6d6d
YashK-27 <kankriyayash18@gmail.com>
2024-01-28 19:47:53 +0530

    Data Ingestion

--- src/logger.py
+++ src/logger.py
@@ -1,7 +1,6 @@
 import logging
 import os
 from datetime import datetime
-from src.exception import CustomException
 
 LOG_FILE = f"{datetime.now().strftime('%m_%d_%Y_%H_%M_%S')}.log"
 log_path = os.path.join(os.getcwd(), "logs", LOG_FILE)
@@ -14,11 +13,3 @@ logging.basicConfig(
     format="[ %(asctime)s ] %(lineno)d %(name)s - %(levelname)s - %(message)s",
     level=logging.INFO,  # message with severity level INFO or higher will be logged
 )
-
-
-
-# def main():  
-#     logging.info("This is an example log message.")
-
-# if __name__ == '__main__':
-#     main()
